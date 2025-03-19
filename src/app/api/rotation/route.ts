3f4691380b85e761b78916ff9824464ab0038148
Eletsia <minjh2749@gmail.com>
2025-03-19 09:44:38 +0900

    fix:error

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -27,6 +27,7 @@ export async function GET() {
     const data = await response.json();
     return NextResponse.json(data);
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { error: "Internal server error" },
       { status: 500 }
